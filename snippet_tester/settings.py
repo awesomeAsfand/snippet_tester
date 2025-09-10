cc217ca93052a252ec19be13db7230860a379e4b
asfand <asfand.asfand@gmail.com>
2025-09-11 02:43:50 +0500

    implemented social Authentication

--- snippet_tester/settings.py
+++ snippet_tester/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = "django-insecure-m-wy3_p74&1jrdh^9diywt_x+3uph7s6b@_k4($v63-n74-or#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sinppet-tester.com', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['sinppet-tester.com', 'localhost', '127.0.0.1']
 
-
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    # 'django.contrib.auth.backends.ModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
     # 'account.authentication.EmailAuthBackend',
     'social_core.backends.google.GoogleOAuth2',
 ]
