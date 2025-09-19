30ded31fb601b2842ab25ca80d7176893e1f62a5
asfand <asfand.asfand@gmail.com>
2025-09-19 07:12:40 +0500

    modified multi-form save operations

--- testt/views.py
+++ testt/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseForbidden
 from django.middleware.csrf import get_token
 from django.shortcuts import render, redirect
 from django.template.defaulttags import csrf_token
 from django.views.decorators.http import require_http_methods
-
+from django.contrib.auth.decorators import login_required
 from .forms import TestForm, PageForm, SnippetVariantFormSet, SnippetVariant
 from .models import Page, SnippetVariant, Test
 from django.shortcuts import get_object_or_404, render
+from django.db import transaction
+
 
 def create_test(request):
     if request.method == "POST":
@@ -15,23 +18,27 @@ def create_test(request):
         variant_formset = SnippetVariantFormSet(request.POST, queryset=SnippetVariant.objects.none())
 
         if test_form.is_valid() and page_form.is_valid() and variant_formset.is_valid():
-            page = page_form.save(commit=False)
-            page.user = request.user
-            page.save()
+            try:
+                with transaction.atomic():
+                    page = page_form.save(commit=False)
+                    page.user = request.user
+                    page.save()
 
-            test = test_form.save(commit=False)
-            test.page = page
-            test.status = "created"
-            test.save()
+                    test = test_form.save(commit=False)
+                    test.page = page
+                    test.status = "created"
+                    test.save()
 
-            for form in variant_formset:
-                variant = form.save(commit=False)
-                variant.page = page
-                variant.save()
+                    for form in variant_formset:
+                        if form.has_changed():  # ← Only save if user actually entered something
+                            variant = form.save(commit=False)
+                            variant.page = page
+                            variant.save()
 
-            # Redirect to dashboard
-            return redirect("dashboard:dashboard")
-
+                    # Redirect to dashboard
+                    return redirect("dashboard:dashboard")
+            except Exception:
+                pass
     else:
         test_form = TestForm()
         page_form = PageForm()
@@ -80,14 +87,14 @@ def update_status(request, test_id):
     return HttpResponse(select_html)
 
 
+@login_required()
+@require_http_methods(['DELETE'])
 def delete_test(request, test_id):
     get_test = get_object_or_404(Test, pk=test_id)
-    if request.method == 'POST':
-        get_test.delete()
-    return HttpResponse(204)
-
-
-
-
-
+    page = get_test.page
+    if get_test.page.user != request.user:
+        return HttpResponseForbidden('You cannot delete this')
+    else:
+        page.delete()
+    return HttpResponse(status=204)
 
